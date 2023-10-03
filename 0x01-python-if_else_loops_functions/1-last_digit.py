31f8aa7fe2dde383154c8d55bfcbcc2a4d1078b5
rayden <rayden.ai@gmail.com>
2023-10-03 13:52:42 +0100

    Auto-format using black

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,10 @@ import random
 
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
+
+if number < 0:
+    last = -last
+
 if last > 5:
     print(f"Last digit of {number:d} is {last:d} and is greater than 5")
 elif last < 6 and last != 0:
